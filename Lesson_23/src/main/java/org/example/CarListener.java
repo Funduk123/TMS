fe245ab64a2d0d53ca6341e6fea2d2c9aae2c00f
Danila Rebkovets <danila.rebkovets20@gmail.com>
2022-12-22 19:02:05 +0300

    Lesson_23_v2.3

--- Lesson_23/src/main/java/org/example/CarListener.java
+++ Lesson_23/src/main/java/org/example/CarListener.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.io.PrintWriter;
 
 @WebListener
 public class CarListener implements HttpSessionListener {
--- Lesson_23/src/main/java/org/example/CarsServlet.java
+++ Lesson_23/src/main/java/org/example/CarsServlet.java
@@ -5,10 +5,7 @@ import org.example.service.CarsList;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -32,8 +29,19 @@ public class CarsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        String id = req.getParameter("id");
 
+        HttpSession session = req.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+
+        if (count == null) {
+            count = 1;
+            session.setAttribute("count", 1);
+        } else {
+            session.setAttribute("count", count + 1);
+        }
+        resp.getOutputStream().println("Count of visits: " + count);
+
+        String id = req.getParameter("id");
         if (id == null) {
             resp.getOutputStream().println(carsList.getCarsList().toString());
         } else {
